39177ac166d66c86498d33c3fbdf27c59e4386b6
GUSTAVO MATOS RODRIGUES <35430775+gustavomatosrodrigues@users.noreply.github.com>
2020-12-12 12:10:59 -0300

    testees
    testes

--- banco.php
+++ banco.php
@@ -1,3 +1,4 @@
 <?php
 	function conecta(){
-	$link = mysqli_connect("externo.logosystem.com.br","root","logokanzeon", "auxilium");
+	$link = mysqli_connect('externo.logosystem.com.br','root', 'logokanzeon', 'auxilium');
+?>
