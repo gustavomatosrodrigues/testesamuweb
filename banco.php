3c54919b667474f3cc9df5f84cb538ac1aae94ba
GUSTAVO MATOS RODRIGUES <35430775+gustavomatosrodrigues@users.noreply.github.com>
2020-12-12 12:15:11 -0300

    testes
    testes

--- banco.php
+++ banco.php
@@ -11,17 +11,19 @@
 		// Retorna conexão
 		//return $mysql;
 
-	$link = mysqli_connect("externo.logosystem.com.br","root","logokanzeon", "auxilium");
+		$link = mysqli_connect("externo.logosystem.com.br","root","logokanzeon", "auxilium");
 
-	if (!$link) {
-		echo "Error: Unable to connect to MySQL." . PHP_EOL;
-		echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-		echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-		exit;
+		if (!$link) {
+			echo "Error: Unable to connect to MySQL." . PHP_EOL;
+			echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+			echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+			exit;
+		}
+
+		echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+		echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+		mysqli_close($link);
+
 	}
-
-	echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-	echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-	mysqli_close($link);
 ?>
