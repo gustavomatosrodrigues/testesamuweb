7e9811c1ca64b4c3d0d8bf7063e1969f19b13440
GUSTAVO MATOS RODRIGUES <35430775+gustavomatosrodrigues@users.noreply.github.com>
2020-12-12 11:59:34 -0300

    testes
    testes

--- banco.php
+++ banco.php
@@ -1,14 +1,31 @@
 <?php
 	function conecta(){
 		// Remoto
-		$mysql = mysql_connect("externo.logosystem.com.br","root","logokanzeon");
-		mysql_select_db("auxilium",$mysql);
-		mysql_set_charset("utf8",$mysql);
+		//$mysql = mysql_connect("externo.logosystem.com.br","root","logokanzeon");
+		//mysql_select_db("auxilium",$mysql);
+		//mysql_set_charset("utf8",$mysql);
 		// Local
 		/*$mysql = mysql_connect("127.0.0.1","root","root");
 		mysql_select_db("auxilium",$mysql);
 		mysql_set_charset("utf8",$mysql);*/
 		// Retorna conexão
-		return $mysql;
+		//return $mysql;
+
+	$link = mysqli_connect("externo.logosystem.com.br","root","logokanzeon", "auxilium");
+
+	if (!$link) {
+		echo "Error: Unable to connect to MySQL." . PHP_EOL;
+		echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+		echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+		exit;
 	}
+
+	echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+	echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+	//mysqli_close($link);
+
+
+		
+	
 ?>
